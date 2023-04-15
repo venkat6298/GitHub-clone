86abf1e32aa08a8c4d37974e6c76d0854c26a174
venkatesh <venkateshwarraovodapally@gmail.com>
2023-04-15 18:21:25 -0400

    Initial Commit

---
+++ GitHub clone/GitHub_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_cloneApp.swift
+//  GitHub clone
+//
+//  Created by Venkateshwar Rao on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
